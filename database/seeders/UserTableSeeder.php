27b005bf68ce8e45fc59fb255f08b4ecff75e5f0
robykartis <robykarti.s@gmail.com>
2022-05-07 18:39:07 +0700

    commit

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
 
 class UserTableSeeder extends Seeder
 {
@@ -14,6 +18,15 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'Super Roby',
+            'email' => 'roby@admin.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('superadmin'),
+            'remember_token' => Str::random(10),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+
+        ]);
     }
 }
